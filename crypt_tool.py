0ab90a64f742ffde9931bf83679cfdaaa03a36eb
pgigoux <pgigoux@gemini.edu>
2024-01-13 10:47:32 -0300

    New command line option

--- crypt_tool.py
+++ crypt_tool.py
@@ -10,17 +10,30 @@ if __name__ == '__main__':
 
     # Process command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', action='store', type=str, help='Input string')
+
+    parser.add_argument(dest='input',
+                        action='store',
+                        type=str,
+                        default='',
+                        help='Input string')
+
+    parser.add_argument('-d', '--decrypt',
+                        dest='decrypt',
+                        action='store_true',
+                        default=False,
+                        help='Decrypt string? [default=False]')
+
     args = parser.parse_args()
 
     # Initialize encryption/decryption with password from user
     password = get_password()
     c = Crypt(password)
 
-    # Try to encrypt/decrypt the string
-    # Decryption is expected to fail with ill-formed encrypted strings
-    print(f'enc: [{c.encrypt_str2str(args.input)}]')
-    try:
-        print(f'dec: [{c.decrypt_str2str(args.input)}]')
-    except Exception as e:
-        print('decryption failed', e)
+    # Decryption is expected to fail with ill-formed encrypted strings or password mismatch
+    if args.decrypt:
+        try:
+            print(f'[{c.decrypt_str2str(args.input)}]')
+        except Exception as e:
+            print('decryption failed', e)
+    else:
+        print(f'[{c.encrypt_str2str(args.input)}]')
