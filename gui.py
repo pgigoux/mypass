7b7a0a798d7e8ed54f98e2164ff7f77c6ad00560
pgigoux <pgigoux@gemini.edu>
2024-04-05 19:44:30 -0300

    Add item list

--- gui.py
+++ gui.py
@@ -4,7 +4,8 @@ import tkinter as tk
 from tkinter import filedialog as tk_fd
 from tkinter import messagebox as tk_mb
 from crypt import Crypt
-from command import CommandProcessor, Response
+from command import CommandProcessor
+from response import Response, Severity
 
 GUI_WIDTH = 900
 GUI_HEIGHT = 700
@@ -43,12 +44,14 @@ class Gui:
         self.directory = Path.home()
         self.file_name = tk.StringVar(value=NO_FILE_NAME)
         self.password = tk.StringVar(value='')
+        # items
+        self.item_mapping = {}
+        self.item_listbox = None  # listbox
         # self.password.trace('w', self.dump_trace)
-        #
         self.cp = CommandProcessor(self.confirm_dialog, self.get_encryption_key)
 
-    def dump_trace(self, variable_name: str, index: str, operation: str):
-        print('trace', variable_name, operation, self.password.get(), index)
+    # def dump_trace(self, variable_name: tk.Variable, index: str, operation: str):
+    #     print('trace', variable_name., operation, variable_name.get(), index)
 
     def get_encryption_key(self) -> Crypt | None:
         p = self.password.get()
@@ -69,6 +72,10 @@ class Gui:
         :return: True if Yes, False if No.
         """
         return tk_mb.askyesno('Confirmation', text + '\n\n' + 'Do you want to proceed?')
+
+    @staticmethod
+    def message_dialog(r: Response):
+        tk_mb.showinfo(str(r.severity), str(r.value))
 
     def file_dialog(self, title: str, file_type: tuple | None, ok_action: Callable, password_flag=True):
         """
@@ -112,16 +119,25 @@ class Gui:
         print('action', self.file_name.get(), self.password.get())
         w.destroy()
 
+    def open_menu_callback(self):
+        self.file_dialog('Open', ('Database', '*.db'), self.open_action)
+
+    def get_item_list(self) -> list:
+        r = self.cp.item_list()
+        if r.is_ok and r.is_list:
+            return [(x[0], x[1]) for x in sorted(r.value, key=lambda x: x[1].lower())]
+        else:
+            return []
+
     def open_action(self, w: tk.Toplevel):
         w.destroy()
         print('action', self.file_name.get(), self.password.get())
         r = self.cp.database_read(self.file_name.get())
-        print(r)
-        print(self.cp.item_list())
-        # print(self.confirm_dialog('Database already exists'))
-
-    def open_menu_callback(self):
-        self.file_dialog('Open', ('Database', '*.db'), self.open_action)
+        if r.is_ok:
+            print(r)
+            self.init_list_frame(self.get_item_list())
+        else:
+            self.message_dialog(r)
 
     def search_callback(self):
         print('search callback', self.search_pattern.get())
@@ -168,15 +184,61 @@ class Gui:
 
         tk.Button(f, text="Search", command=self.search_callback).pack(fill='x', side=tk.LEFT)
 
-        tk.Checkbutton(f, text="Name", var=self.search_name).pack(fill='x', side=tk.LEFT)
-        tk.Checkbutton(f, text="Tag", var=self.search_tag).pack(fill='x', side=tk.LEFT)
-        tk.Checkbutton(f, text="Field name", var=self.search_field_name).pack(fill='x', side=tk.LEFT)
-        tk.Checkbutton(f, text="Field value", var=self.search_field_value).pack(fill='x', side=tk.LEFT)
-        tk.Checkbutton(f, text="Note", var=self.search_note).pack(fill='x', side=tk.LEFT)
+        tk.Checkbutton(f, text="Name", variable=self.search_name).pack(fill='x', side=tk.LEFT)
+        tk.Checkbutton(f, text="Tag", variable=self.search_tag).pack(fill='x', side=tk.LEFT)
+        tk.Checkbutton(f, text="Field name", variable=self.search_field_name).pack(fill='x', side=tk.LEFT)
+        tk.Checkbutton(f, text="Field value", variable=self.search_field_value).pack(fill='x', side=tk.LEFT)
+        tk.Checkbutton(f, text="Note", variable=self.search_note).pack(fill='x', side=tk.LEFT)
 
         f.pack(fill=tk.X, side=tk.TOP, expand=False)  # do not expand in y
 
         return f
+
+    def item_select(self, event):
+        n = self.item_listbox.curselection()[0]
+        print(n, self.item_mapping[n], event)
+
+    def init_list_frame(self, item_list: list):
+        self.item_listbox = tk.Listbox(self.list_frame, width=30, bg="yellow", selectmode=tk.SINGLE,
+                                       activestyle='dotbox', fg="black")
+        scrollbar = tk.Scrollbar(self.list_frame, orient=tk.VERTICAL, command=self.item_listbox.yview)
+        self.item_listbox['yscrollcommand'] = scrollbar.set
+
+        self.item_mapping = {}
+
+        n = 0
+        for i_id, i_name, in item_list:
+            print(n, i_id, i_name)
+            self.item_mapping[n] = i_id
+            self.item_listbox.insert(n, f'{i_id:03d}  {i_name}')
+            n += 1
+
+        scrollbar.pack(side=tk.RIGHT, expand=True, fill=tk.Y)
+        self.item_listbox.bind('<<ListboxSelect>>', self.item_select)
+        self.item_listbox.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+
+    # def init_list_frame(self):
+    #
+    #     self.item_listbox = tk.Listbox(self.list_frame, width=30, bg="yellow", selectmode=tk.SINGLE,
+    #                                    activestyle='dotbox', fg="black")
+    #     scrollbar = tk.Scrollbar(self.list_frame, orient=tk.VERTICAL, command=self.item_listbox.yview)
+    #     self.item_listbox['yscrollcommand'] = scrollbar.set
+    #
+    #     self.item_mapping = {}
+    #
+    #     n = 0
+    #     r = self.cp.item_list()
+    #     if r.is_ok and r.is_list:
+    #         item_list = r.value
+    #         for i_id, i_name, _, _, in item_list:
+    #             print(n, i_id, i_name)
+    #             self.item_mapping[n] = i_id
+    #             self.item_listbox.insert(n, f'{i_id: 3d}  {i_name}')
+    #             n += 1
+    #
+    #     scrollbar.pack(side=tk.RIGHT, expand=True, fill=tk.Y)
+    #     self.item_listbox.bind('<<ListboxSelect>>', self.item_select)
+    #     self.item_listbox.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
 
     def start_gui(self):
         self.root.title('Mypass')
